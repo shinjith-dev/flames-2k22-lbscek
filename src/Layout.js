534dedfcfbe78d1c9e990d6eff89f578dd1fd914
the3plet <soorajp2002@gmail.com>
2022-12-08 22:12:06 +0530

    tabs added

--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./componenets/Header";
 import Footer from './componenets/Footer'
 
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Container,Tabs,Tab} from 'react-bootstrap/'
+
+
+
 
 const Events = () => {
   return (
-    <div>Events</div>
+    <div>
+      <Container>
+      <Tabs
+      defaultActiveKey="men"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="men" title="Men">
+        Items for mens
+      </Tab>
+      <Tab eventKey="women" title="Women">
+        Items for women
+      </Tab>
+    </Tabs>
+      </Container>
+    </div>
   )
 }
 
