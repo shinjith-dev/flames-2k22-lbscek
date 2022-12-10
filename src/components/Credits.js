d32076013683eec1949a40490b307404093f1ac3
the3plet <soorajp2002@gmail.com>
2022-12-10 12:59:36 +0530

    Credit design changed

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -6,12 +6,35 @@ import ListGroup from "react-bootstrap/ListGroup";
 const popover = (
   <Popover id="popover-basic">
     <Popover.Body>
-      <p className="text-center mb-1">
-      This website was created in collaboration with{" "}
-        <strong>TinkerHub LBSCEK</strong> & <strong>MuLearn LBSCEK</strong>
-      </p>
+      <div className="text-center mb-1">
+      <strong>Created in collaboration with</strong>
+      <a
+          href="https://instagram.com/tinkerhub.lbscek"
+          class="d-inline-block py-2 p-1 text-decoration-none"
+        >
+          <img
+            alt="TinkerHub Logo"
+            src="https://media.discordapp.net/attachments/883626663160729601/1051033216494084168/IMG_20221210_124025.jpg"
+            className="rounded-rectangle me-1"
+            style={{ width: "4rem", height: "4rem" }}
+          />
+          
+        </a>
+        <a
+          href="https://instagram.com/gtechmulearn"
+          class="d-inline-block py-2 p-1 text-decoration-none"
+        >
+          <img
+            alt="Mulearn Logo"
+            src="https://media.discordapp.net/attachments/883626663160729601/1051036455079772160/IMG_20221210_125315.png"
+            className="rounded-rectangle me-1"
+            style={{ width: "4rem", height: "4rem" }}
+          />
+          
+        </a>
+      </div>
 
-      <p className="text-center mb-0">Core Contributors:</p>
+      <p className="text-center mb-0">Contributors:</p>
       <div className="text-center">
         <a
           href="https://github.com/the3plet/"
@@ -37,6 +60,19 @@ const popover = (
           />
           Shinjith
         </a>
+        <a
+          href="https://github.com/shinjith-dev/"
+          class="d-inline-block py-2 p-1 text-decoration-none"
+        >
+          <img
+            alt="Anjali-img"
+            src="https://t4.ftcdn.net/jpg/02/45/28/17/360_F_245281721_2uYVgLSFnaH9AlZ1WWpkZavIcEEGBU84.jpg"
+            className="rounded-circle me-1"
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+          Anjali
+        </a>
+       
       </div>
     </Popover.Body>
   </Popover>
