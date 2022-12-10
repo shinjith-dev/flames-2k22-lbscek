1c7c77e24f1590c01c840789ff38483588b575b6
shinjith-dev <shinjithpallikkara@gmail.com>
2022-12-10 11:29:25 +0530

    popover update 7 font change

--- src/components/ResultPopup.js
+++ src/components/ResultPopup.js
@@ -1,30 +1,44 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { useState,useEffect } from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { useState, useEffect } from "react";
 
+const ResultPopup = ({ item, closePopup }) => {
+  const [show, setShow] = useState({ show: true, value: item });
+  useEffect(() => {
+    setShow((prev) => ({ ...prev, show: true, value: item }));
+  }, []);
 
-const ResultPopup = ({item,closePopup})=>{
-  const [show, setShow] = useState({show:true,value:item});
-useEffect(()=>{
-  setShow(prev=>({...prev,show:true,value:item}))
-},[])
-
-  const handleClose = () => {closePopup(); setShow(false);}
+  const handleClose = () => {
+    closePopup();
+    setShow(false);
+  };
   return (
     <>
       <Offcanvas show={show} onHide={handleClose} placement="bottom">
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title className='fw-bold'>{item[0][0]}</Offcanvas.Title>
+          <Offcanvas.Title className="fw-bold">{item[0][0]}</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>{show.value[0][1]==='Nil'?<p>Not finished yet</p>:<>          <p>{`First: ${item[0][1]} - ${item[1][1]}`}</p>
-          <p>{`Second: ${show.value[0][2]} - ${show.value[1][2]}`}</p>
-          <p>{`Third: ${show.value[0][3]} - ${show.value[1][3]}`}</p></>}
-
+        <Offcanvas.Body className="py-0">
+          {show.value[0][1] === "Nil" ? (
+            <p className="fw-bold">Not finished yet</p>
+          ) : (
+            <>
+              <div>
+                First:<p className="fw-bold my-1">{` ${item[0][1]} - ${item[1][1]}`}</p>
+              </div>
+              <div>
+                Second:<p className="fw-bold my-1">{` ${show.value[0][2]} - ${show.value[1][2]}`}</p>
+              </div>
+              <div>
+                Third:<p className="fw-bold my-1">{` ${show.value[0][3]} - ${show.value[1][3]}`}</p>
+              </div>
+            </>
+          )}
         </Offcanvas.Body>
       </Offcanvas>
     </>
   );
-}
+};
 
 export default ResultPopup;
